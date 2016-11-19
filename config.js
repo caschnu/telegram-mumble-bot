004c8d5c15aa32d112edaacd2fbd376e26e5c332
Wilko Theessen Meyer <binaryspace@binaryworkspace.fritz.box>
2016-11-19 20:29:54 +0100

    Klaas update rollback of rollback

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ config.WEBHOOK_BASE_URL = process.env.WEBHOOK_BASE_URL;
 config.WEBHOOK_PATH = process.env.WEBHOOK_PATH || '/telegramBot';
 config.TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 config.TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+config.TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID;
 config.MUMBLE_URL = process.env.MUMBLE_URL;
 config.MUMBLE_USER = process.env.MUMBLE_USER;
 config.MUMBLE_PASSWORD = process.env.MUMBLE_PASSWORD;
